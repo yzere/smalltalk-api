c5c315bfde3ccda2d5da2c32be45f2d4df4d4c9d
Karol Godlewski <karolugod@gmail.com>
2022-02-06 19:23:14 +0100

    Checking if webhook is well configured.

--- api/models.py
+++ api/models.py
@@ -41,7 +41,7 @@ class User(AbstractBaseUser):
     #                                         unique=True)
     email                = models.EmailField(verbose_name='email', max_length=255, unique=True)
 
-    username             = models.CharField(max_length=255, unique=True)
+    username             = models.CharField(max_length=254, unique=True)
     date_joined          = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
     last_login           = models.DateTimeField(verbose_name='last login', auto_now=True)
     is_admin             = models.BooleanField(default=False)
