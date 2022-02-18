e7b5af31311753154b262b08024aad2cd261f6f3
wayson14 <wayson.1104@gmail.com>
2022-02-18 12:00:18 +0100

    Dodany skrypt startujący dev server i aplikujący migracje (uwaga, venv musi się nazywać venv, w przeciwnym wypadku trzeba zmienić nazwę w skrpycie).

---
+++ full_start.sh
@@ -0,0 +1,4 @@
+source ~/Projects/smalltalk-api/venv/bin/activate
+python ~/Projects/smalltalk-api/manage.py makemigrations
+python ~/Projects/smalltalk-api/manage.py migrate
+python ~/Projects/smalltalk-api/manage.py runserver
