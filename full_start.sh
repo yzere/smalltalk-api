6695253b4eeaafac4aef7e9b7573ed9cef28f42d
wayson14 <43252091+wayson14@users.noreply.github.com>
2022-02-18 12:17:00 +0100

    Dodanie instrukcji obsługi do pliku ze skryptem autostartującym.

--- full_start.sh
+++ full_start.sh
@@ -1,3 +1,9 @@
+# Jeżeli chcemy zrobić alias pozwalający na odpalanie tego z dowolnego miejsca w systemi za pomocą jednego polecenia, należy
+# dopisać poniższą linię do pliku "~/.bashrc" na końcu a następnie wpisać "source ~/.bashrc". 
+
+#   alias full_api_start="source $HOME/Projects/smalltalk-api/full_start.sh"
+
+#!/usr/bin/bash
 source ~/Projects/smalltalk-api/venv/bin/activate
 python ~/Projects/smalltalk-api/manage.py makemigrations
 python ~/Projects/smalltalk-api/manage.py migrate
