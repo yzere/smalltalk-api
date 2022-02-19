0eba187ca40705357256c8b23913712759c3c7d0
wayson14 <wayson.1104@gmail.com>
2022-02-19 14:07:55 +0100

    Dodanie devowych originów do listy zaufanych.

--- backend/settings.py
+++ backend/settings.py
@@ -56,10 +56,13 @@ INSTALLED_APPS = [
 
 # ----------------------- Authentication Stuff -----------------------
 CORS_ALLOWED_ORIGINS = [    
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://127.0.0.1:3000',
+    'http://localhost:3000',
+    'https://localhost:3000',
 ]
 CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = ["127.0.0.1:3000"]
+CSRF_TRUSTED_ORIGINS = ["127.0.0.1:3000", "localhost:3000"]
 # SESSION_COOKIE_SAMESITE = 'None'
 # CSRF_COOKIE_SAMESITE = 'None'
 
