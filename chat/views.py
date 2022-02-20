ff5fcc406704e0a3bb665b2b9961ff25a1d8a6a6
Kac321 <kacper.capiga@tm1.edu.pl>
2022-02-20 16:46:22 +0100

    Automatyczne dodawanie użytkownika to WaitingRoom, zapisywanie w ActiveSession messages.ID

--- chat/views.py
+++ chat/views.py
@@ -7,16 +7,20 @@ def index(request):
 
     # Jak na jeżeli są jakieś sesje, lub nie ma sesji, to dodaje użytkownika lub tworzy sesję, pozostało: jeśeli użytkownik ma sesję to do niej dodaje
 
-    user_id = request.user
-    addWaiting = WaitingRoom(user_that_want_to_join_ID=user_id)
+    user_id = request.user.user_ID
+    isWaiting = WaitingRoom.objects.get(user_that_want_to_join_ID=user_id)
+    if not isWaiting:
+        addWaiting = WaitingRoom(user_that_want_to_join_ID=user_id)
+        addWaiting.save()
     freeSessions = ActiveSession.objects.filter(member2_ID__isnull=True).values_list('pk', flat=True)
+    print(freeSessions)
 
-    if freeSessions  and not WaitingRoom.objects.get(user_that_want_to_join_ID=user_id):
+    if freeSessions  and not ActiveSession.objects.filter(member2_ID=user_id):
         randomSessionPk = choice(freeSessions)
         randomSessionObj = ActiveSession.objects.get(pk=randomSessionPk)
         randomSessionObj.member2_ID = WaitingRoom.objects.get(user_that_want_to_join_ID=user_id)
         randomSessionObj.save()
-    elif not WaitingRoom.objects.get(user_that_want_to_join_ID=user_id):
+    elif not ActiveSession.objects.filter(member1_ID=user_id) and not ActiveSession.objects.filter(member2_ID=user_id):
         newSession = ActiveSession(member1_ID=WaitingRoom.objects.get(user_that_want_to_join_ID=user_id))
         newSession.save()
 
