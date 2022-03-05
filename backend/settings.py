7450b3eab960ac9bee6907a8ce750908842239f1
Kac321 <kacper.capiga@tm1.edu.pl>
2022-03-05 13:19:52 +0100

    Zmiana metody hashowania haseł, do logowania się można używać starej bazy danych ze starą metodą ,ale każdy mowo utworzony użytkownik używa Argon2 z domyślnymi ustawieniami Django (aby działało trzeba zainstalować pakiety argona, które są w req.txt)

--- backend/settings.py
+++ backend/settings.py
@@ -190,3 +190,10 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels.layers.InMemoryChannelLayer"
     }
 }
+
+# Password hashers for auth
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher'
+]
